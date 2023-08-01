0386740a333596083f64e4c511872c1ad8241b8e
msaddamhk <saddamhk567@gmail.com>
2023-08-01 17:08:58 +0700

    revisi4

--- app/Http/Controllers/user/BerandaController.php
+++ app/Http/Controllers/user/BerandaController.php
@@ -5,17 +5,15 @@ namespace App\Http\Controllers\user;
 use App\Http\Controllers\Controller;
 use App\Models\Galeri;
 use App\Models\Jasa;
-use App\Models\Pemesanan;
 use App\Models\Slider;
-use Illuminate\Http\Request;
 
 class BerandaController extends Controller
 {
     public function index()
     {
-        $jasa = Jasa::paginate(4);
-        $galeri = Galeri::paginate(8);
-        $slider = Slider::paginate(8);
+        $jasa = Jasa::latest()->paginate(4);
+        $galeri = Galeri::latest()->paginate(8);
+        $slider = Slider::latest()->paginate(8);
         return view('home.index', compact('jasa', 'galeri', 'slider'));
     }
 }
